7e71229b0600454ac9901dc66b64c9459de0a9b7
codeclub <codeclub@smms-rm23-24.smusd.local>
2018-02-07 14:57:22 -0800

    robot squar

--- src/RobotSquare.java
+++ src/RobotSquare.java
@@ -11,22 +11,23 @@ public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
     	// 1. Make a new Robot
-
+Robot Dj = new Robot();
 
         // 3. Put the robot's pen down
-
+Dj.penDown();
 
         // 6. Make the robot move as fast as possible
-
+Dj.setSpeed(10);
 
         // 5. Do everything below here 4 times
-
+for(int i = 0; i< 4; i++) {
 
         //         2. Move your robot 200 pixels
-
+Dj.move(200);
 
         //         4. Turn the robot 90 degrees to the right (90 degrees)
-
+Dj.turn(90);
 
     }
+}
 }
